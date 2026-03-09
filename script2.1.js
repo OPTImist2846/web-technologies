a496734557c49a2fb66163dd1b2171a959720afe
OPTImist2846 <olessolja@gmail.com>
2026-03-09 19:40:26 +0200

    feam: add script2.1 function check range

---
+++ script2.1.js
@@ -0,0 +1,13 @@
+function checkRange(num, min, max) {
+    
+    if (num >= min && num <= max) {
+        console.log("Число " + num + " знаходиться в діапазоні від " + min + " до " + max);
+        return true;
+    } else {
+        console.log("Число " + num + " НЕ в діапазоні.");
+        return false;
+    }
+}
+
+checkRange(10, 5, 20);
+checkRange(3, 5, 20); 
