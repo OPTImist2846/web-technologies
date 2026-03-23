c0e889c16683b49e8dc11fe6104669615654849f
OPTImist2846 <olessolja@gmail.com>
2026-03-23 21:40:46 +0200

    add task2 function

--- Half2/Lab4/script.js
+++ Half2/Lab4/script.js
@@ -14,3 +14,18 @@ function task1() {
     let index = fruits.indexOf("яблуко");
     console.log("4. Індекс 'яблуко':", index);
     }
+
+    function task2() {
+    console.log("\n=== Завдання 2 ===");
+    let colors = ["червоний", "жовтий", "синій", "зелений", "темно-синій"];
+    
+    let longest = colors.reduce((a, b) => a.length >= b.length ? a : b);
+    let shortest = colors.reduce((a, b) => a.length <= b.length ? a : b);
+    console.log(`2. Найдовший: ${longest}, Найкоротший: ${shortest}`);
+    
+    let blueColors = colors.filter(color => color.includes("синій"));
+    console.log("3. Кольори зі словом 'синій':", blueColors);
+    
+    let joinedColors = blueColors.join(", ");
+    console.log("4-5. Об'єднаний рядок:", joinedColors);
+}
