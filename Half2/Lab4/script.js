a2e75493620704f95c47c29aa8e267029d0271dd
OPTImist2846 <olessolja@gmail.com>
2026-03-23 21:36:17 +0200

    add script.js: pop, unshift, reverse, index

--- Half2/Lab4/script.js
+++ Half2/Lab4/script.js
@@ -1,4 +1,16 @@
 function task1() {
     console.log("=== Завдання 1 ===");
     let fruits = ["яблуко", "банан", "груша", "ківі"];
+
+    fruits.pop();
+    console.log("1. Оновлений масив (без останнього):", fruits);
+    
+    fruits.unshift("ананас");
+    console.log("2. Додано 'ананас':", fruits);
+    
+    let sortedFruits = [...fruits].sort().reverse(); 
+    console.log("3. Зворотний алфавітний порядок:", sortedFruits);
+    
+    let index = fruits.indexOf("яблуко");
+    console.log("4. Індекс 'яблуко':", index);
     }
